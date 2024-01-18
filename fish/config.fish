c1a56b5d07c984511ab74a144f0f19de8619fcec
Miguel <mige.d@outlook.com>
2024-01-17 21:38:20 -0500

    add fish config

---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+set fish_greeting
+set EDITOR 'nvim'
+
+# Aliases
+abbr ls 'eza'
+abbr ll 'eza -l'
+abbr la 'eza -la'
+
+abbr cat 'bat'
+abbr top 'btop'
+abbr vim 'nvim'
+abbr g 'git'
+
+starship init fish | source
